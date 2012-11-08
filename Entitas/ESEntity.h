a41f9fdcde7ece0c672cebe70afa9735b2a04383
Maxim Zaks <maxim.zaks@audiotool.com>
2012-11-08 16:56:49 +0100

    added macros and explanations

--- Entitas/ESEntity.h
+++ Entitas/ESEntity.h
@@ -1,7 +1,15 @@
 #import <Foundation/Foundation.h>
 #import "ESComponent.h"
 
+// Handy macro to get an component from entity
+// Example: PositionComponent *pos = getComponent(e, PositionComponent);
 #define getComponent(entity, ComponentType) (ComponentType *)[entity getComponentOfType:[ComponentType class]];
+
+// A very lazy programmer can use this macro.
+// It not only gets the component from entity but also defines the local variable
+// Example: defineComponent(pos, e, PositionComponent)
+// Transforms to: PositionComponent *pos = (PositionComponent*)[e getComponentOfType:[PositionComponent class]]
+#define defineComponent(name, entity, ComponentType) ComponentType * name = (ComponentType *)[entity getComponentOfType:[ComponentType class]];
 
 @class ESEntities;
 
