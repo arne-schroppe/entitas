84c61fda8213cf867469a7379fa78defc2984525
Arne Schroppe <arne.schroppe@wooga.net>
2013-08-08 14:42:07 +0200

    Fixed macros

--- Entitas/ESAllComponentTypes.h
+++ Entitas/ESAllComponentTypes.h
@@ -2,7 +2,7 @@
 #import "ESComponentMatcher.h"
 #import "ESAbstractComponentMatcher.h"
 
-#define allComponentsOfTypes(...) ([[GXAllComponentTypes alloc] initWithClasses: __VA_ARGS__ ])
+#define allComponentsOfTypes(...) ([[ESAllComponentTypes alloc] initWithClasses: __VA_ARGS__ ])
 
 @interface ESAllComponentTypes : ESAbstractComponentMatcher
 @end
--- Entitas/ESAnyComponentTypes.h
+++ Entitas/ESAnyComponentTypes.h
@@ -2,7 +2,7 @@
 #import "ESComponentMatcher.h"
 #import "ESAbstractComponentMatcher.h"
 
-#define anyComponentsOfTypes(...) ([[GXAnyComponentType alloc] initWithClasses: __VA_ARGS__ ])
+#define anyComponentsOfTypes(...) ([[ESAnyComponentType alloc] initWithClasses: __VA_ARGS__ ])
 
 @interface ESAnyComponentTypes : ESAbstractComponentMatcher
 @end
